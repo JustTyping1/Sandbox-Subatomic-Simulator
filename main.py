4c1ce0d108ec4472e9adacc0be71f6610dc93a55
JustTyping1 <aditya.chorghade@outlook.com>
2023-10-27 12:16:23 +0100

    window

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame
 
+pygame.init()
+window_size = (800, 600)
+window = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Subatomic Simulator!")
+
+running = True
+while running:
+   for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+         running = False
+    
+   pygame.display.flip()
+pygame.quit()
