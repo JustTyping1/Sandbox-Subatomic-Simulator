a31e70358260bb09afc500560aee30461ac4c830
JustTyping1 <aditya.chorghade@outlook.com>
2023-10-28 08:54:13 +0100

    created proton init class

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pygame
 
+class Proton(xpos, ypos, xvel, yvel, xacc, yacc):
+   def __init__(self, xpos, ypos, xvel, yvel, xacc, yacc):
+      self.xpos = xpos
+      self.ypos = ypos
+      self.xvel = xvel
+      self.yvel = yvel
+      self.xacc = xacc
+      self.yacc = yacc
+
+
 pygame.init()
 window_size = (800, 600)
 window = pygame.display.set_mode(window_size)
