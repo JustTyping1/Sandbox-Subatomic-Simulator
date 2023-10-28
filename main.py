c058d9a71d88288002567a5d759476a97f1c676c
JustTyping1 <aditya.chorghade@outlook.com>
2023-10-28 12:44:27 +0100

    added neutron

--- main.py
+++ main.py
@@ -26,6 +26,18 @@ class Electron(xpos, ypos, xvel, yvel, xacc, yacc):
       self.charge = -0.00000000000000000016
       self.mass = 0.00000000000000000000000000000091093837
 
+class Neturon(xpos, ypos, xvel, yvel, xacc, yacc):
+   def __init__(self, xpos, ypos, xvel, yvel, xacc, yacc):
+      # Adding variables for the proton's velocity, acceleration, position, charge and mass
+      self.xpos = xpos
+      self.ypos = ypos
+      self.xvel = xvel
+      self.yvel = yvel
+      self.xacc = xacc
+      self.yacc = yacc
+      self.charge = 0 
+      self.mass = 0.00000000000000000000000000000091093837 + 0.0000000000000000000000000167262192
+
 pygame.init()
 window_size = (800, 600)
 window = pygame.display.set_mode(window_size)
