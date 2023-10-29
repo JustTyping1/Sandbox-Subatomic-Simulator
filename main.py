549767dc07b0f7a5ad43afab4df7f8e1233c480d
SaganashDev <saganashdev@gmail.com>
2023-10-29 14:58:17 +0000

    Abstraction and unit changes
    Abstracted motion variables from class initialisers into a general motion state class (Motion2D) and further abstracted to a 2D vector class. Also changed units to ones which are more suitable for the application (elementary charge and MeV/c^2).

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 import pygame
+from typing import *
+
+eCharge = NewType("eCharge", float) # 1.602176634 10^{-19} C # Elementary charge
+MeVPerCSquare = NewType("MeVPerCSquare", float) # MeV/c^2 # Common unit used in particle physics
+
+class Vector2D():
+   def __init__(self, x: float, y: float) -> None:
+      self.x: float = x
+      self.y: float = y
+
+class Motion2D():
+   def __init__(self, pos: Vector2D, vel: Vector2D = Vector2D(0, 0), acc: Vector2D = Vector2D(0, 0)):
+      self.xpos: float = pos.x
+      self.ypos: float = pos.y
+      self.xvel: float = vel.x
+      self.yvel: float = vel.y
+      self.xacc: float = acc.x
+      self.yacc: float = acc.y
 
 # Class for Proton subatomic particle
 class Proton():
-   def __init__(self, xpos, ypos, xvel, yvel, xacc, yacc):
-      # Adding variables for the proton's velocity, acceleration, position, charge and mass
-      self.xpos = xpos
-      self.ypos = ypos
-      self.xvel = xvel
-      self.yvel = yvel
-      self.xacc = xacc
-      self.yacc = yacc
-      self.charge = 0.00000000000000000016
-      self.mass = 0.00000000000000000000000000167262192
+   def __init__(self, initMotion: Motion2D):
+      self.state: Motion2D = initMotion
+
+      self.charge: eCharge = eCharge(1)
+      self.mass: MeVPerCSquare = MeVPerCSquare(938.28)
+
    def render(self):
       imp = pygame.image.load("protonsprite.png").convert()
-      window.blit(imp, (self.xpos, self.ypos))
+      window.blit(imp, (self.state.xpos, self.state.ypos))
 
 # Class for Electron subatomic particle
 class Electron():
-   def __init__(self, xpos, ypos, xvel, yvel, xacc, yacc):
-      # Adding variables for the proton's velocity, acceleration, position, charge and mass
-      self.xpos = xpos
-      self.ypos = ypos
-      self.xvel = xvel
-      self.yvel = yvel
-      self.xacc = xacc
-      self.yacc = yacc
-      self.charge = -0.00000000000000000016
-      self.mass = 0.00000000000000000000000000000091093837
+   def __init__(self, initMotion: Motion2D):
+      self.state: Motion2D = initMotion
+
+      self.charge: eCharge = eCharge(-1)
+      self.mass: MeVPerCSquare = MeVPerCSquare(0.511)
+
    def render(self):
       imp = pygame.image.load("electronsprite.png").convert()
-      window.blit(imp, (self.xpos, self.ypos))
+      window.blit(imp, (self.state.xpos, self.state.ypos))
 
 # Class for Neutron subatomic particle
 class Neturon():
-   def __init__(self, xpos, ypos, xvel, yvel, xacc, yacc):
-      # Adding variables for the proton's velocity, acceleration, position, charge and mass
-      self.xpos = xpos
-      self.ypos = ypos
-      self.xvel = xvel
-      self.yvel = yvel
-      self.xacc = xacc
-      self.yacc = yacc
-      self.charge = 0 
-      self.mass = 0.00000000000000000000000000000091093837 + 0.0000000000000000000000000167262192
+   def __init__(self, initMotion: Motion2D):
+      self.state: Motion2D = initMotion
+
+      self.charge: eCharge = eCharge(0)
+      self.mass: MeVPerCSquare = MeVPerCSquare(939.57)
+
    def render(self):
       imp = pygame.image.load("neutronsprite.png").convert()
-      window.blit(imp, (self.xpos, self.ypos))
+      window.blit(imp, (self.state.xpos, self.state.ypos))
 
 pygame.init()
 window_size = (800, 600)
@@ -60,15 +66,15 @@ while running:
          running = False
    
    # Rendering differnet particles at coords (test)
-   proton1 = Proton(50, 50, 0, 0, 0, 0)
+   proton1 = Proton(Motion2D(Vector2D(50, 50)))
    proton1.render()
-   electron1 = Electron(200, 300, 0, 0, 0, 0)
-   electron1.render()
-   neutron1 = Neturon(600, 400, 0, 0, 0, 0)
-   neutron1.render()
-   
 
-    
+   electron1 = Electron(Motion2D(Vector2D(200, 300)))
+   electron1.render()
+
+   neutron1 = Neturon(Motion2D(Vector2D(600, 400)))
+   neutron1.render()
+
    pygame.display.flip()
 
 pygame.quit()
